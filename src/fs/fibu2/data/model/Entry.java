0714b7e532dcedf131e3ad0b6e64902892e9e980
Simon Hampe <simon.hampe@googlemail.com>
2009-03-28 18:50:44 +0000

    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@103 200f3aa7-6921-4cde-8511-3937f5af78e3

---
+++ src/fs/fibu2/data/model/Entry.java
@@ -0,0 +1,42 @@
+package fs.fibu2.data.model;
+
+import org.dom4j.Element;
+import org.dom4j.Node;
+
+import fs.xml.XMLConfigurable;
+import fs.xml.XMLReadConfigurationException;
+import fs.xml.XMLWriteConfigurationException;
+
+public class Entry implements XMLConfigurable {
+
+	// MEMBERS ******************************
+	
+	private String name;
+	
+	
+	
+	@Override
+	public String getIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isConfigured() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void configure(Node arg0) throws XMLWriteConfigurationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Element getConfiguration() throws XMLReadConfigurationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
