93fb6a8e8c654955e15ca2fe89d30674f765f9db
Simon Hampe <simon.hampe@googlemail.com>
2009-05-22 18:03:36 +0000

    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@181 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/fibu2/test/PreferencesTest.java
+++ src/fs/fibu2/test/PreferencesTest.java
@@ -1,6 +1,7 @@
 package fs.fibu2.test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
@@ -10,18 +11,8 @@ public class PreferencesTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Preferences p = Preferences.userRoot();
-		Preferences n = p.node("bla");
-		n.put("id", "ff2filter_name");
-		try {
-			p.exportSubtree(System.out);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Arrays.copyOf(new int[5], 3);
+		
 		
 	}
 
