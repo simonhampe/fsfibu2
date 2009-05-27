2a9fe1269dbc4481f7c75bf887a32e3a4be96531
Simon Hampe <simon.hampe@googlemail.com>
2009-05-27 08:01:29 +0000

    - Wrote the TableModel interface part of JournalTableModel (which, I am afraid, was the easy part) - Created some strings for column names
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@188 200f3aa7-6921-4cde-8511-3937f5af78e3

---
+++ src/fs/fibu2/view/model/BilancialInformation.java
@@ -0,0 +1,35 @@
+package fs.fibu2.view.model;
+
+import java.util.HashMap;
+
+import fs.fibu2.data.model.Account;
+import fs.fibu2.data.model.Category;
+
+/**
+ * A BilancialInformation is a tupel of bilancial sums for a fsfibu2 journal table row. It contains sums for: <br>
+ * - The sum of all entries (up to a certain point)<br>
+ * - The sum of all entries in certain categories <br>
+ * - The sum of all entries in certain accounts <br>
+ * All elements can be publicly modified
+ * @author Simon Hampe
+ *
+ */
+public class BilancialInformation {
+
+	/**
+	 * The overall sum of all entries
+	 */
+	public int overallSum = 0;
+	
+	/**
+	 * Sums of entries in certain categories
+	 */
+	public HashMap<Category, Integer> categorySums = new HashMap<Category, Integer>();
+	
+	/**
+	 * Sums of entries using certain accounts
+	 */
+	public HashMap<Account, Integer> accountSums = new HashMap<Account, Integer>();
+	
+	
+}
--- src/fs/fibu2/view/model/JournalTableModel.java
+++ src/fs/fibu2/view/model/JournalTableModel.java
@@ -1,15 +1,25 @@
 package fs.fibu2.view.model;
 
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Vector;
+
 import javax.swing.SwingWorker;
+import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
 import fs.fibu2.data.event.JournalListener;
 import fs.fibu2.data.model.Account;
+import fs.fibu2.data.model.Category;
 import fs.fibu2.data.model.Entry;
+import fs.fibu2.data.model.EntrySeparator;
 import fs.fibu2.data.model.Journal;
+import fs.fibu2.data.model.LinkedSeparator;
 import fs.fibu2.data.model.ReadingPoint;
 import fs.fibu2.filter.StackFilter;
+import fs.fibu2.lang.Fsfibu2StringTableMgr;
 
 /**
  * This class implements a model for a Journal table. Each model is associated to a {@link StackFilter} to which is listens, as well as to the associated {@link Journal}.
@@ -21,52 +31,95 @@ import fs.fibu2.filter.StackFilter;
  */
 public class JournalTableModel implements TableModel, JournalListener {
 
+	//Data ******
+	
+	//The list of row objects
+	Vector<Object> listOfRows = new Vector<Object>();
+	//The set of linked separators
+	HashSet<LinkedSeparator> linkedSeparators = new HashSet<LinkedSeparator>();
+	//The list of bilancial information (in 1:1-correspondance to listOfRows)
+	Vector<HashMap<EntrySeparator, BilancialInformation>> bilancialRows = new Vector<HashMap<EntrySeparator, BilancialInformation>>();
+	
+	//Listeners ******
+	
+	private HashSet<TableModelListener> listenerList = new HashSet<TableModelListener>();
+	
+	//Misc ******
+	
+	private final static String sgroup = "fs.fibu2.model.JournalTableModel";
+	
+	// CONSTRUCTOR **************************************
+	// **************************************************
+	
+	// TABLEMODEL ***************************************
+	// **************************************************
+	
 	@Override
 	public void addTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-
+		if(l != null) listenerList.add(l);
 	}
 
+	/**
+	 * @return Object.class for all
+	 */
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return Object.class;
 	}
 
+	/**
+	 * @return 8
+	 */
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 8;
 	}
 
+	/**
+	 * @return A descriptive name for the given column
+	 */
 	@Override
 	public String getColumnName(int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		switch(columnIndex) {
+		case 0: return Fsfibu2StringTableMgr.getString(sgroup + ".columnIndic");
+		case 1: return Fsfibu2StringTableMgr.getString(sgroup + ".columnName");
+		case 2: return Fsfibu2StringTableMgr.getString(sgroup + ".columnDate");
+		case 3: return Fsfibu2StringTableMgr.getString(sgroup + ".columnValue");
+		case 4: return Fsfibu2StringTableMgr.getString(sgroup + ".columnAccount");
+		case 5: return Fsfibu2StringTableMgr.getString(sgroup + ".columnCategory");
+		case 6: return Fsfibu2StringTableMgr.getString(sgroup + ".columnAccInfo");
+		case 7: return Fsfibu2StringTableMgr.getString(sgroup + ".columnAddInfo");
+		default: return "";
+		}
 	}
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return listOfRows.size();
 	}
 
+	/**
+	 * @return The entry or separator in the row, regardless of the columnIndex
+	 */
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		if(rowIndex < 0 || rowIndex >= listOfRows.size()) {
+			throw new ArrayIndexOutOfBoundsException("Illegal row argument: " + rowIndex);
+		}
+		return listOfRows.get(rowIndex);
 	}
 
+	/**
+	 * @return false
+	 */
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void removeTableModelListener(TableModelListener l) {
-		// TODO Auto-generated method stub
-
+		listenerList.remove(l);
 	}
 
 	/**
@@ -75,6 +128,13 @@ public class JournalTableModel implements TableModel, JournalListener {
 	@Override
 	public void setValueAt(Object value, int rowIndex, int columnIndex) {
 		throw new UnsupportedOperationException("Cannot modify journal from model");
+	}
+	
+	// LISTENER MECHANISM ***********************************************
+	// ******************************************************************
+	
+	protected void fireTableChanged(TableModelEvent e) {
+		for(TableModelListener l : listenerList) l.tableChanged(e);
 	}
 	
 	// JOURNALLISTENER **************************************************
