e2ab8ce986c86dd4c7faa15e7f3f0aec4f5097b1
Simon Hampe <simon.hampe@googlemail.com>
2009-06-30 10:18:50 +0000

    Started writing Fsfibu2 and SplashScreenManager
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@237 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/fibu2/application/Fsfibu2.java
+++ src/fs/fibu2/application/Fsfibu2.java
@@ -1,13 +1,53 @@
 package fs.fibu2.application;
 
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * This class is responsible for initializing and starting fsfibu2. The expected initialization files (which are  all optional) are: <br>
+ * - frameworkConfigurator.xml: This file configurates fsframework, mainly the path to fsframework. If the file does not exist, the user is prompted to
+ * give the appropriate directory. The application will not start, if the user does not give a valid directory.<br>
+ * - loggerConfigurator: This file configurates logging. It is supposed to be in a format as specified by {@link PropertyConfigurator}. If it is not present, 
+ * a basic configuration is used <br>
+ * All files should be located in the working directory.
+ * @author Simon Hampe
+ *
+ */
 public class Fsfibu2 {
 
+	private static Logger logger = Logger.getLogger("fs.fibu2");
+	
 	/**
-	 * @param args
+	 * Initializes and starts fsfibu2. Command line arguments are ignored.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//First configure logging
+		File f = new File("loggerConfigurator");
+		if(f.exists()) {
+			PropertyConfigurator.configure(f.getAbsolutePath());
+			logger.info("Logging configured via " + f.getName());
+		}
+		else {
+			BasicConfigurator.configure();
+			logger.info("No logging configuration file found. Basic configuration used.");
+		}
+		
+		//Now configure fsframework
+		try {
+			FrameworkLoader.loadFramework();
+		}
+		catch(UnsupportedOperationException e) {
+			JOptionPane.showMessageDialog(null, "Could not find fsframework. fsfibu2 will not be started", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		//Now load all user classes
+		
+		
 	}
 
 }
---
+++ src/fs/fibu2/application/SplashScreenManager.java
@@ -0,0 +1,20 @@
+package fs.fibu2.application;
+
+import java.awt.SplashScreen;
+
+import org.apache.log4j.Appender;
+
+/**
+ * This class manages the splash screen. It can be used as an {@link Appender} for logging while initializing fsfibu2.
+ * @author Simon Hampe
+ *
+ */
+public class SplashScreenManager {
+
+	private static Appender appender = 
+	
+	public Appender getSplashScreenAppender() {
+		
+	}
+	
+}
