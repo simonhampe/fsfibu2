a19b08dfdb36b4843158a645dfc61036ae376c7b
Simon Hampe <simon.hampe@googlemail.com>
2009-07-17 11:39:45 +0000

    Continued on BilancialTreeModel
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@283 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/fibu2/view/model/BilancialTreeModel.java
+++ src/fs/fibu2/view/model/BilancialTreeModel.java
@@ -1,15 +1,50 @@
 package fs.fibu2.view.model;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Vector;
+
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
-public class BilancialTreeModel implements TreeModel {
+import fs.fibu2.data.event.JournalListener;
+import fs.fibu2.data.model.Account;
+import fs.fibu2.data.model.Category;
+import fs.fibu2.data.model.Entry;
+import fs.fibu2.data.model.Journal;
+import fs.fibu2.data.model.ReadingPoint;
 
+/**
+ * This class implements a tree model displaying  the categories actually used in a journal (plus an additional child for each category which has 
+ * subcategories AND entries directly in this category).
+ * @author Simon Hampe
+ *
+ */
+public class BilancialTreeModel implements TreeModel, JournalListener {
+
+	// DATA ************************
+	// *****************************
+	
+	//The number of entries in each used category (for keeping track of node removal and addition)
+	private HashMap<Category, Integer> numberOfEntries = new HashMap<Category, Integer>();
+	//The direct subcategories of each used category
+	private HashMap<Category, Vector<Category>> directSubcategories = new HashMap<Category, Vector<Category>>();
+	
+	//The corr. journal
+	private Journal associatedJournal;
+	
+	private HashSet<TreeModelListener> listenerList = new HashSet<TreeModelListener>();
+	
+	// CONSTRUCTOR *****************
+	// *****************************
+	
+	// TREEMODEL *******************
+	// *****************************
+	
 	@Override
 	public void addTreeModelListener(TreeModelListener arg0) {
-		// TODO Auto-generated method stub
-
+		if(arg0 != null) listenerList.add(arg0);
 	}
 
 	@Override
@@ -44,14 +79,80 @@ public class BilancialTreeModel implements TreeModel {
 
 	@Override
 	public void removeTreeModelListener(TreeModelListener arg0) {
-		// TODO Auto-generated method stub
+		listenerList.remove(arg0);
+	}
 
+	/**
+	 * Ignored, since values should only be changed via the corresponding journal.
+	 */
+	@Override
+	public void valueForPathChanged(TreePath arg0, Object arg1) {
+		//Ignored
+	}
+
+	// JOURNALLISTENER ************************
+	// ****************************************
+	
+	@Override
+	public void descriptionChanged(Journal source, String oldValue,
+			String newValue) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public void valueForPathChanged(TreePath arg0, Object arg1) {
+	public void entriesAdded(Journal source, Entry[] newEntries) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void entriesRemoved(Journal source, Entry[] oldEntries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void entryReplaced(Journal source, Entry oldEntry, Entry newEntry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nameChanged(Journal source, String oldValue, String newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readingPointAdded(Journal source, ReadingPoint point) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readingPointRemoved(Journal source, ReadingPoint point) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startValueChanged(Journal source, Account a, Float oldValue,
+			Float newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dateChanged(ReadingPoint source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nameChanged(ReadingPoint source) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
