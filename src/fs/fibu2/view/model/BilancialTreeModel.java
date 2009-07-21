d8d57abaa51bf378fc21cfd042b5ff577b35adcd
Simon Hampe <simon.hampe@googlemail.com>
2009-07-21 09:30:23 +0000

    - Changed BilancialTreeModel to use an extended version of Categories - Started to write model calculation
    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@287 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/fibu2/view/model/BilancialTreeModel.java
+++ src/fs/fibu2/view/model/BilancialTreeModel.java
@@ -2,13 +2,17 @@ package fs.fibu2.view.model;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.Vector;
 import java.util.prefs.Preferences;
 
+import javax.swing.SwingWorker;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import fs.fibu2.data.event.JournalListener;
 import fs.fibu2.data.model.Account;
@@ -17,6 +21,7 @@ import fs.fibu2.data.model.Entry;
 import fs.fibu2.data.model.Journal;
 import fs.fibu2.data.model.ReadingPoint;
 import fs.fibu2.filter.StackFilter;
+import fs.fibu2.view.event.ProgressListener;
 
 /**
  * This class implements a tree model displaying  the categories actually used in a journal (plus an additional child for each category which has 
@@ -36,10 +41,10 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	// *****************************
 	
 	//All categories contained
-	private HashSet<Category> used = new HashSet<Category>();
+	private HashSet<ExtendedCategory> used = new HashSet<ExtendedCategory>();
 	
 	//The direct subcategories of each used category
-	private HashMap<Category, Vector<Category>> directSubcategories = new HashMap<Category, Vector<Category>>();
+	private HashMap<ExtendedCategory, Vector<ExtendedCategory>> directSubcategories = new HashMap<ExtendedCategory, Vector<ExtendedCategory>>();
 	
 	//The bilancials for each category, including subcategories (the last is of course the sum of the first two)
 	private HashMap<Category, Float> plus = new HashMap<Category, Float>();
@@ -55,19 +60,19 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	private HashMap<Account, Float> before = new HashMap<Account, Float>();
 	private HashMap<Account, Float> after = new HashMap<Account, Float>();
 	
-	//Each category contained in here is invisble. The second set contains the additional category node which
-	//	 is inserted, whenever a category has subcategories, but also entries in the category itself.
-	private HashSet<Category> invisbles = new HashSet<Category>();
-	private HashSet<Category> invisibleIndiv = new HashSet<Category>();
+	//Each category contained in here is invisble. 
+	private HashSet<ExtendedCategory> invisibles = new HashSet<ExtendedCategory>();
 	
-	//The mask of each category (null = there is no mask). The second set contains the additional nodes
-	private HashMap<Category, String> mask = new HashMap<Category, String>();
-	private HashMap<Category, String> maskIndiv = new HashMap<Category, String>();
+	//The mask of each category (null = there is no mask). 
+	private HashMap<ExtendedCategory, String> mask = new HashMap<ExtendedCategory, String>();
 	
 	//The corresponding journal
 	private Journal associatedJournal;
+	//The filter used by this model
+	private StackFilter filter;
 	
 	private HashSet<TreeModelListener> listenerList = new HashSet<TreeModelListener>();
+	private HashSet<ProgressListener<Object, Object>> progressListeners = new HashSet<ProgressListener<Object,Object>>();
 	
 	// CONSTRUCTOR *****************
 	// *****************************
@@ -78,6 +83,60 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 */
 	public BilancialTreeModel(Journal j, StackFilter f, Preferences node) {
 		
+	}
+	
+	// RECALCULATION ***************
+	// *****************************
+	
+	protected DataVector recalculateModel() {
+		DataVector v = new DataVector();
+		
+		//Whether an entry has already been accepted by the filter
+		boolean entriesAccepted = false;
+		//The bilancial of all entries 'before'
+		BilancialInformation biBefore = new BilancialInformation();
+		//The overall bilancial of all accepted entries (divided into minus, plus, sum)
+		BilancialInformation biAcceptedPlus = new BilancialInformation();
+		BilancialInformation biAcceptedMinus = new BilancialInformation();
+		BilancialInformation biAcceptedSum = new BilancialInformation();
+		//The overall bilancial off all 'before' and accepted entries
+		BilancialInformation biOverall = new BilancialInformation();
+		//The sorted set of all entries
+		TreeSet<Entry> entries = new TreeSet<Entry>(new TableModelComparator());
+			entries.addAll(associatedJournal.getEntries());
+		for(Entry e : entries) {
+			if(filter.verifyEntry(e)) {
+				entriesAccepted = true;
+				//Increment bilancial
+				
+				if(e.getValue() >= 0) biAcceptedPlus.increment(e);
+				else biAcceptedMinus.increment(e);
+				
+				biAcceptedSum.increment(e);
+				biOverall.increment(e);
+				
+				//Add category
+				
+			}
+			else {
+				if(!entriesAccepted) {
+					biBefore.increment(e);
+					biOverall.increment(e);
+				}
+			}
+		}
+		
+		//Copy data
+		for(Category c : biAccepted.getCategoryMappings().keySet()) {
+			v.used.add(new ExtendedCategory(c,false));
+			
+		}
+		
+		v.before = biBefore.getAccountMappings();
+		v.after = biOverall.getAccountMappings();
+		
+		
+		return v;
 	}
 	
 	// GETTERS & SETTERS ***********
@@ -136,7 +195,7 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 * there are subcategories AND entries in a category, but only the overall node of this category
 	 */
 	public boolean isVisible(Category c) {
-		return invisbles.contains(c);
+		return invisibles.contains(new ExtendedCategory(c,false));
 	}
 	
 	/**
@@ -144,31 +203,32 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 * whenever there is no such node
 	 */
 	public boolean isVisibleIndiv(Category c) {
-		return invisibleIndiv.contains(c);
+		return invisibles.contains(new ExtendedCategory(c,true));
 	}
 	
 	/**
 	 * @return The mask of the given category (NOT the additional child node) or null, if this category is not masked
 	 */
 	public String getMask(Category c) {
-		return mask.get(c);
+		return mask.get(new ExtendedCategory(c,false));
 	}
 	
 	/**
 	 * @return The mask of the additional node for the given category or null, if the node is not masked or does not exist
 	 */
 	public String getIndividualMask(Category c) {
-		return maskIndiv.get(c);
+		return mask.get(new ExtendedCategory(c,true));
 	}
 	
 	/**
 	 * Sets the visibility of the given category node (not the additional node). The root category cannot be made invisible.
 	 */
 	public void setVisibility(Category c, boolean visible) {
-		if(c != null && c != Category.getRootCategory() && used.contains(c)) {
-			if(visible) invisbles.remove(c);
-			else invisbles.add(c);
-			fireTreeNodesChanged(new TreeModelEvent(this,getPath(c.parent),new int[]{getIndex(c)},new Object[]{c}));
+		ExtendedCategory ec = new ExtendedCategory(c,false);
+		if(c != null && c != Category.getRootCategory() && used.contains(ec)) {
+			if(visible) invisibles.remove(ec);
+			else invisibles.add(ec);
+			fireTreeNodesChanged(new TreeModelEvent(this,getPath(new ExtendedCategory(c.parent,false)),new int[]{getIndex(ec)},new Object[]{ec}));
 		}
 	}
 	
@@ -176,10 +236,12 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 * Sets the visibility of the additional node in the given category (if it exists)
 	 */
 	public void setIndividualVisibility(Category c, boolean visible) {
-		if(c != null && used.contains(c) &&  directSubcategories.get(c).contains(c)) {
-			if(visible) invisibleIndiv.remove(c);
-			else invisibleIndiv.add(c);
-			fireTreeNodesChanged(new TreeModelEvent(this,getPath(c).pathByAddingChild(c),new int[]{0}, new Object[]{c}));
+		ExtendedCategory ec = new ExtendedCategory(c,true);
+		ExtendedCategory ecp = new ExtendedCategory(c,false);
+		if(c != null && used.contains(ecp) &&  directSubcategories.get(ecp).contains(ec)) {
+			if(visible) invisibles.remove(ec);
+			else invisibles.add(ec);
+			fireTreeNodesChanged(new TreeModelEvent(this,getPath(ecp),new int[]{getIndex(ec)}, new Object[]{ec}));
 		}
 	}
 	
@@ -187,10 +249,12 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 * Sets the mask of the given category node (not the additional node). If mask == null, the mask is removed
 	 */
 	public void setMask(Category c, String  maskString) {
-		if(c != null && used.contains(c)) {
-			if(maskString == null) mask.remove(c);
-			else mask.put(c, maskString);
-			fireTreeNodesChanged(new TreeModelEvent(this, getPath(c.parent), new int[]{getIndex(c)},new Object[]{c}));
+		ExtendedCategory ec = new ExtendedCategory(c,false);
+		if(c != null && used.contains(ec)) {
+			ExtendedCategory ecp = new ExtendedCategory(c == Category.getRootCategory()? c : c.parent,false);
+			if(maskString == null) mask.remove(ec);
+			else mask.put(ec, maskString);
+			fireTreeNodesChanged(new TreeModelEvent(this, getPath(ecp), new int[]{getIndex(ec)},new Object[]{ec}));
 		}
 	}
 	
@@ -198,10 +262,12 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 	 * Sets the mask of the additional node in the given category, if it exists. A null value removes the mask
 	 */
 	public void setIndividualMask(Category c, String maskString) {
-		if(c != null && used.contains(c) && directSubcategories.get(c).contains(c)) {
-			if(maskString == null) maskIndiv.remove(c);
-			else maskIndiv.put(c, maskString);
-			fireTreeNodesChanged(new TreeModelEvent(this, getPath(c),new int[]{0},new Object[]{c}));
+		ExtendedCategory ec = new ExtendedCategory(c,true);
+		ExtendedCategory ecp = new ExtendedCategory(c,false);
+		if(c != null && used.contains(ec) && directSubcategories.get(ecp).contains(ec)) {
+			if(maskString == null) mask.remove(ec);
+			else mask.put(ec, maskString);
+			fireTreeNodesChanged(new TreeModelEvent(this, getPath(ecp),new int[]{0},new Object[]{ec}));
 		}
 	}
 	
@@ -239,7 +305,7 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 
 	@Override
 	public Object getRoot() {
-		return Category.getRootCategory();
+		return new ExtendedCategory(Category.getRootCategory(),false);
 	}
 
 	@Override
@@ -347,22 +413,135 @@ public class BilancialTreeModel implements TreeModel, JournalListener {
 		for(TreeModelListener l : listenerList) l.treeStructureChanged(e);
 	}
 	
+	protected void fireTaskBegins(SwingWorker<Object, Object> source) {
+		for(ProgressListener<Object, Object> l : progressListeners) l.taskBegins(source);
+	}
+	
+	protected void fireProgressed(SwingWorker<Object, Object> source) {
+		for(ProgressListener<Object, Object> l : progressListeners) l.progressed(source);
+	}
+	
+	protected void fireTaskFinished(SwingWorker<Object, Object> source) {
+		for(ProgressListener<Object, Object> l : progressListeners) l.taskFinished(source);
+	}
+	
+	public void addProgressListener(ProgressListener<Object, Object> l) {
+		if(l != null) progressListeners.add(l);
+	}
+	
+	public void removeProgressListener(ProgressListener<Object, Object> l) {
+		progressListeners.remove(l);
+	}
+	
+	// HELPER METHODS **************************
+	// *****************************************
+	
 	/**
 	 * @return The tree path to the given category (or null, if this tree does not contain the category)
 	 */
-	protected TreePath getPath(Category c) {
+	protected TreePath getPath(ExtendedCategory c) {
 		if(c == null  || !used.contains(c)) return null;
-		if(c == Category.getRootCategory()) return new TreePath(Category.getRootCategory());
-		else return getPath(c.parent).pathByAddingChild(c);
+		if(c.isAdditional()) {
+			return (new TreePath(new ExtendedCategory(c.category,false))).pathByAddingChild(c);
+		}
+		else {
+			if(c.category == Category.getRootCategory()) return new TreePath(c);
+			else return (new TreePath(new ExtendedCategory(c.category.parent,false))).pathByAddingChild(c);
+		}
 	}
 	
 	/**
 	 * @return The index of the given category in its parent category (-1, if this tree does not contain the category and 0 for root)
 	 */
-	protected int getIndex(Category c) {
+	protected int getIndex(ExtendedCategory c) {
 		if(c == null || !used.contains(c)) return -1;
-		if(c == Category.getRootCategory()) return 0;
-		else return directSubcategories.get(c.parent).indexOf(c);
+		if(c.isAdditional()) return 0;
+		else {
+			return directSubcategories.get(new ExtendedCategory(c.category.parent,false)).indexOf(c);
+		}
+	}
+	
+	// LOCAL CLASSES ***************************
+	// *****************************************
+	
+	/**
+	 * This class represents a category, together with the additional information, whether this is a
+	 *  'real' category node or the additional category node in the category itself (a boolean flag: It is true, when 
+	 *  the object is the additional node)
+	 */
+	public final class ExtendedCategory {
+		private Category category;
+		private boolean isAdditional;
+		
+		/**
+		 * Creates an extended category
+		 * @param c The category
+		 * @param add Whether this node is the additional node in a category
+		 */
+		public ExtendedCategory(Category c, boolean add) {
+			category = c;
+			isAdditional = add;
+		}
+		
+		public Category category() {
+			return category;
+		}
+		
+		public boolean isAdditional() {
+			return isAdditional;
+		}
+		
+		/**
+		 * Two extended categories are equal, if and only if their categories are equal and the isAdditional flag has the same value
+		 */
+		@Override
+		public boolean equals(Object o) {
+			if(!(o instanceof ExtendedCategory)) return false;
+			else return (((ExtendedCategory)o).category == category && ((ExtendedCategory)o).isAdditional == isAdditional);
+		}
+		
+		@Override
+		public int hashCode() {
+			return category.hashCode() ^ (new Boolean(isAdditional)).hashCode();
+		}
+	}
+	
+	/**
+	 * Used for the data transfer of the newly calculated data
+	 * @author Simon Hampe
+	 *
+	 */
+	private class DataVector {
+		public HashSet<ExtendedCategory> used = new HashSet<ExtendedCategory>();
+		public HashMap<ExtendedCategory, Vector<ExtendedCategory>> directSubcategories = new HashMap<ExtendedCategory, Vector<ExtendedCategory>>();
+		public HashMap<ExtendedCategory, Float> minus = new HashMap<ExtendedCategory, Float>();
+		public HashMap<ExtendedCategory, Float> plus = new HashMap<ExtendedCategory, Float>();
+		public HashMap<ExtendedCategory, Float> sum = new HashMap<ExtendedCategory, Float>();
+		public HashMap<ExtendedCategory, Float> minusIndiv = new HashMap<ExtendedCategory, Float>();
+		public HashMap<ExtendedCategory, Float> plusIndiv = new HashMap<ExtendedCategory, Float>();
+		public HashMap<ExtendedCategory, Float> sumIndiv = new HashMap<ExtendedCategory, Float>();
+		public HashMap<Account, Float> before = new HashMap<Account, Float>();
+		public HashMap<Account, Float> after = new HashMap<Account, Float>();
+	}
+	
+	/**
+	 * Recalculator class used for recalculating the model in a separate thread
+	 * @author Simon Hampe
+	 *
+	 */
+	private class Recalculator extends SwingWorker<DataVector, DataVector> {
+
+		@Override
+		protected DataVector doInBackground() throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected void done() {
+			// TODO Auto-generated method stub
+			super.done();
+		}
 	}
 
 }
