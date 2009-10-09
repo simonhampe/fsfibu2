d829b81250a8a96fbf1179933d6c24fe62a3ec61
Simon Hampe <simon.hampe@googlemail.com>
2009-10-09 14:42:37 +0000

    git-svn-id: file:///home/simon/tmp/svnmigrate/svn/fsfibu2@391 200f3aa7-6921-4cde-8511-3937f5af78e3

--- src/fs/fibu2/test/PreferencesTest.java
+++ src/fs/fibu2/test/PreferencesTest.java
@@ -3,6 +3,14 @@ package fs.fibu2.test;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
+import javax.swing.JFrame;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.util.SortOrder;
+
 /**
  * Theoretically I wrote this class to test the {@link Preferences} class - but didn't really need it, so I'm using it for small one-time tests.
  * @author Simon Hampe
@@ -15,7 +23,20 @@ public class PreferencesTest {
 	 * @throws BackingStoreException 
 	 */
 	public static void main(String[] args) throws BackingStoreException {
-					
+			JFrame frame = new JFrame();
+			
+			DefaultPieDataset dataset = new DefaultPieDataset();
+				dataset.setValue("Süßigkeiten", 10);
+				dataset.setValue("Fachschaft", 20);
+				dataset.sortByKeys(SortOrder.ASCENDING);
+			PiePlot3D plot = new PiePlot3D(dataset);
+				
+			ChartPanel cframe = new ChartPanel(new JFreeChart(plot));
+			frame.add(cframe);
+			
+			frame.pack();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
 	}
 
 }
